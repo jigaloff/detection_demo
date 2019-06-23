49628fbdb3b946d039f339213fa034fd2c2ee437
jigaloff <jigaloff@gmail.com>
2019-06-23 17:31:07 +0300

    Minimum flask from https://github.com/log0/video_streaming_with_flask_example

--- camera.py
+++ camera.py
@@ -13,13 +13,10 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
     
-    def get_frame(self, image):
-
+    def get_frame(self):
+        success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
-        # ------------------
-
-        # ------------------
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
