537926b5630b48426a608703d7e7fc03196d7e57
jigaloff <jigaloff@gmail.com>
2019-06-25 20:18:04 +0300

    Minimum flask from https://github.com/log0/video_streaming_with_flask_example

--- camera.py
+++ camera.py
@@ -13,10 +13,13 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
     
-    def get_frame(self):
-        success, image = self.video.read()
+    def get_frame(self, image):
+
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
+        # ------------------
+
+        # ------------------
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
--- main.py
+++ main.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python
-#
-# Project: Video Streaming with Flask
-# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
-# Date: 2014/12/21
-# Website: http://www.chioka.in/
-# Description:
-# Modified to support streaming out with webcams, and not just raw JPEGs.
-# Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
-# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-# https://github.com/log0/video_streaming_with_flask_example
-#
-# Usage:
-# 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
-# 2. Run "python main.py".
-# 3. Navigate the browser to the local webpage.
+
+from __future__ import division
+import time
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import cv2
+from util import *
+from darknet import Darknet
+from preprocess import prep_image, inp_to_image
+import pandas as pd
+import random
+import argparse
+import pickle as pkl
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network.
+    Returns a Variable
+    """
+    orig_im = img
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = cv2.resize(orig_im, (inp_dim, inp_dim))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
+
+def write(x, img):
+    c1 = tuple(x[1:3].int())
+    c2 = tuple(x[3:5].int())
+    cls = int(x[-1])
+    label = "{0}".format(classes[cls])
+    color = random.choice(colors)
+    cv2.rectangle(img, c1, c2,color, 1)
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+    cv2.rectangle(img, c1, c2,color, -1)
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    return img
+
+def arg_parse():
+    """
+    Parse arguements to the detect module
+    """
+    parser = argparse.ArgumentParser(description='YOLO v3 Cam Demo')
+    parser.add_argument("--confidence", dest="confidence", help="Object Confidence to filter predictions", default=0.25)
+    parser.add_argument("--nms_thresh", dest="nms_thresh", help="NMS Threshhold", default=0.4)
+    parser.add_argument("--reso", dest='reso', help=
+    "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
+                        default="160", type=str)
+    return parser.parse_args()
 
 app = Flask(__name__)
 
@@ -24,8 +61,31 @@ def index():
     return render_template('index.html')
 
 def gen(camera):
+    i = 0
+    videofile = 'video.avi'
+    classes = load_classes('data/coco.names')
+    colors = pkl.load(open("pallete", "rb"))
+
     while True:
-        frame = camera.get_frame()
+        success, frame = camera.video.read()
+        if i == 1:
+            img, orig_im, dim = prep_image(frame, inp_dim)
+
+            if CUDA:
+                img = img.cuda()
+
+            output = model(Variable(img), CUDA)
+            output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
+
+            output[:, 1:5] = torch.clamp(output[:, 1:5], 0.0, float(inp_dim)) / inp_dim
+            output[:, [1, 3]] *= frame.shape[1]
+            output[:, [2, 4]] *= frame.shape[0]
+
+            list(map(lambda x: write(x, orig_im), output))
+            i = 0
+        i += 1
+        frame = camera.get_frame(frame)
+
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -34,12 +94,17 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
 
     cfgfile = "cfg/yolov3.cfg"
     weightsfile = "yolov3.weights"
     num_classes = 80
+
+    args = arg_parse()
+    confidence = float(args.confidence)
+    nms_thesh = float(args.nms_thresh)
+
     start = 0
     CUDA = torch.cuda.is_available()
 
@@ -50,6 +115,8 @@ if __name__ == '__main__':
 
     model.net_info["height"] = args.reso
     inp_dim = int(model.net_info["height"])
+    classes = load_classes('data/coco.names')
+    colors = pkl.load(open("pallete", "rb"))
 
     assert inp_dim % 32 == 0
     assert inp_dim > 32
@@ -58,3 +125,5 @@ if __name__ == '__main__':
         model.cuda()
 
     model.eval()
+
+    app.run(host='127.0.0.1', debug=True)
